75977d9c118a4ad22e5d0fe1f030d87c06d58139
Sonika Prakash <sonikaprakash10@gmail.com>
2023-12-27 21:01:34 +0530

    added copy message for whatsapp

--- base/utils.go
+++ base/utils.go
@@ -25,6 +25,10 @@ func (a *Application) generateURL(name string) string {
 	return fmt.Sprintf("%s?n=%s", a.server.renderHost, name)
 }
 
+func (a *Application) generateCopyMsg(link string) string {
+	return fmt.Sprintf("Hey have you seen this?\nI have a surprise message for you!\nClick on the below link.\n\n%s\n", link)
+}
+
 func getCapitalizedName(name string) string {
 	return cases.Title(language.English, cases.Compact).String(name)
 }
